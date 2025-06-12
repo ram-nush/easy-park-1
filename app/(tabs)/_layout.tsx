c3c6f7c2182233382b1988d3a0c28f8d2bd28ffb
ram-nush <75689075+ram-nush@users.noreply.github.com>
2025-06-12 12:30:01 +0800

    added map, history and home pages in navigation bar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="map" options={{ title: 'Map View' }} />
+      <Tabs.Screen name="history" options={{ title: 'History' }} />
+      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+    </Tabs>
+  );
+}
