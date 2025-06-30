ca03881ab1b9460f1d852b9af811acba96e89743
Naveen <balkinaveen@gmail.com>
2025-06-30 11:45:00 +0800

    no changes

---
+++ app/(tabs)/history.jsx
@@ -0,0 +1,10 @@
+import styles from '@/styles/styles';
+import { Text, View } from 'react-native';
+
+export default function History() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>History Page</Text>
+    </View>
+  );
+}
